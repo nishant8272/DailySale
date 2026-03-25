0eeb7ac048291a3264d87d94e15eada4b4f9c058
coder1-ctrl <akshanshmittal8@gmail.com>
2026-03-25 22:50:01 +0530

    add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {AuthPage} from "./pages/AuthPage";
 import Products from "./pages/Products";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthPage />} />
+        <Route path="/auth" element={<AuthPage />} />   
         <Route path="/products" element={<Products />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
